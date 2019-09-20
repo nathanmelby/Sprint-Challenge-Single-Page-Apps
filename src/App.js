f44bf6cf06d3039d9920e27427fe47da941d0f0c
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-09-20 10:18:09 -0500

    setting up app with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm";
+
+
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList";
+import LocationsList from "./components/LocationsList";
+
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage}/>
+      <Route exact path="/characterlist" component={CharacterList}/>
+      
     </main>
   );
 }
