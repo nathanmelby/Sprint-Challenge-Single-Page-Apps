fa3fa66fd5430ea2827679e53bbc932fd682e627
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-09-20 13:01:06 -0500

    improved search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,53 @@
-import React from "react";
-// import { Form, Field,} from "formik";
+// import React from "react";
+// // import { Form, Field,} from "formik";
 
-export default function SearchForm() {
+// export default function SearchForm() {
  
+//   return (
+//     <section className="search-form">
+//      <form>
+//        <input
+//        type="search" 
+//        name="search" 
+//        placeholder="Search Here" />
+//        <button>Search</button>
+//      </form>
+//     </section>
+//   );
+// }
+
+
+import React from "react";
+
+
+const characters = [
+ 
+];
+export default function SearchForm() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+  React.useEffect(() => {
+    const results = characters.filter(item =>
+      item.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
   return (
-    <section className="search-form">
-     <form>
-       <input
-       type="search" 
-       name="search" 
-       placeholder="Search Here" />
-       <button>Search</button>
-     </form>
-    </section>
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search Characters"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <ul>
+        {searchResults.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
